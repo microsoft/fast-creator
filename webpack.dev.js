276f24758c5063daa4b1bebfaa61b2e7ae89ce94
William Wagner <44823142+williamw2@users.noreply.github.com>
2021-10-04 11:11:52 -0700

    Enabling symlinking to fast tooling (#47)
    * enable symlinking to fast tooling
    
    * Update docs with link instructions
    
    * Apply suggestions from code review
    
    Co-authored-by: Jane Chu <7559015+janechu@users.noreply.github.com>
    
    Co-authored-by: Jane Chu <7559015+janechu@users.noreply.github.com>

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
+const path = require("path");
 const merge = require("webpack-merge");
 const baseConfigs = require("./webpack.common");
 
@@ -13,5 +14,20 @@ module.exports = baseConfigs.map(baseConfig => {
         output: {
             filename: "[name].js",
         },
+        /* 
+         * Symlinking with fast-tooling requires that all of the fast packages be aliased
+         * to ensure that only the creator version of the package is included. Otherwise
+         * bad stuff happens (like jitRegister errors and loss of component CSS).
+         */
+        resolve:{
+            alias: {
+                "@microsoft/fast-element": path.resolve("node_modules/@microsoft/fast-element"),
+                "@microsoft/fast-foundation": path.resolve("node_modules/@microsoft/fast-foundation"),
+                "@microsoft/fast-components": path.resolve("node_modules/@microsoft/fast-components"),
+                "react": path.resolve("node_modules/react"),
+                "react-dom": path.resolve("node_modules/react-dom"),
+                "react-dnd": path.resolve("node_modules/react-dnd"),
+            }
+        }
     });
 });
