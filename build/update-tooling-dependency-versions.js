4d472739a0c2c3ff721850e6828b3b0935abccee
Jane Chu <7559015+janechu@users.noreply.github.com>
2021-11-11 13:05:16 -0800

    Add workflow and scripting to automatically check and update the stage branch when FAST Tooling dependencies are updated (#100)
    # Pull Request
    
    ## 📖 Description
    
    <!--- Provide some background and a description of your work. -->
    This automated job rubs 1 hour after the FAST Tooling publish. It will check to see if the versions have been updated and if so will update them and run the script to generate new changelog versions for the "What's New" feature.
    
    This should cut down on the possibility that these changes are not added correctly, and that they can now be done in an automated fashion. I have also enabled this for dispatch should it be necessary.
    
    ### 🎫 Issues
    
    <!---
    List and link relevant issues here using the keyword "closes"
    if this PR will close an issue, eg. closes #411
    -->
    Closes #91
    
    ## ✅ Checklist
    
    ### General
    
    <!--- Review the list and put an x in the boxes that apply. -->
    
    - [ ] I have added tests for my changes.
    - [x] I have tested my changes.
    - [x] I have updated the project documentation to reflect my changes.
    
    ## ⏭ Next Steps
    
    <!---
    If there is relevant follow-up work to this PR, please list any existing issues or provide brief descriptions of what you would like to do next.
    -->
    - See if this works, the individual pieces have been tested however the full workflow will need to be checked after packages have been released from FAST Tooling

---
+++ build/update-tooling-dependency-versions.js
@@ -0,0 +1,155 @@
+const { exec } = require("child_process");
+const packageJson = require("../package.json");
+const fs = require("fs");
+const path = require("path");
+
+const dependenciesToUpdate = [
+    "@microsoft/fast-tooling",
+    "@microsoft/fast-tooling-react"
+];
+
+/**
+ * Gets the latest version of the provided package name
+ */
+function getLatestToolingVersions(packageName) {
+    return new Promise((resolve, reject) => {
+        exec(`npm show ${packageName} time --json`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                reject();
+            }
+    
+            if (stderr) {
+                console.error(`stderr: ${stderr}`);
+                reject();
+            }
+    
+            const parsedStdout = JSON.parse(stdout);
+            const packageVersions = Object.keys(parsedStdout);
+    
+            resolve(packageVersions[packageVersions.length - 1]);
+        });
+    });
+}
+
+/**
+ * Gets the current version of the provided package name
+ */
+function getCurrentToolingVersions(packageName) {
+    return packageJson.dependencies[packageName].slice(1);
+}
+
+/**
+ * Checks to see if there are any version updates that need to occur,
+ * if there are, update the package JSON dependencies and the package-lock file
+ */
+async function updateDependencies () {
+   const latestToolingVersions = {};
+   const currentToolingVersions = {};
+   let shouldUpdateVersion = false;
+
+   for (let packageName of dependenciesToUpdate) {
+        await getLatestToolingVersions(packageName).then(async (value) => {
+            latestToolingVersions[packageName] = value;
+        });
+   }
+
+    dependenciesToUpdate.forEach((packageName) => {
+        currentToolingVersions[packageName] = getCurrentToolingVersions(packageName);
+        
+        if (currentToolingVersions[packageName] !== latestToolingVersions[packageName]) {
+            shouldUpdateVersion = true;
+        }
+    });
+
+    if (shouldUpdateVersion) {
+        const updatedPackageJson = packageJson;
+
+        dependenciesToUpdate.forEach((packageName) => {
+            updatedPackageJson.dependencies[packageName] = `^${latestToolingVersions[packageName]}`;
+        });
+        
+        // Update the package.json with the new versions
+        fs.writeFileSync(
+            path.resolve(__dirname, "../package.json"),
+            JSON.stringify(updatedPackageJson, null, 2),
+            {
+                encoding: "utf8"
+            }
+        );
+
+        // Install dependencies to update the package-lock file
+        await new Promise((resolve, reject) => {
+            exec("npm i", (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    reject();
+                }
+        
+                if (stderr) {
+                    console.error(`stderr: ${stderr}`);
+                    reject();
+                }
+
+                resolve();
+            });
+        });
+
+        // Update the version files for the "What's new" overlay
+        await new Promise ((resolve, reject) => {
+            exec("npm run convert:version-files", (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    reject();
+                }
+        
+                if (stderr) {
+                    console.error(`stderr: ${stderr}`);
+                    reject();
+                }
+
+                resolve();
+            });
+        });
+
+        // Stage new file updates
+        await new Promise((resolve, reject) => {
+            exec("git add .", (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    reject();
+                }
+        
+                if (stderr) {
+                    console.error(`stderr: ${stderr}`);
+                    reject();
+                }
+
+                resolve();
+            });
+        }).catch((reason) => {
+            console.error(reason);
+        });
+
+        // Commit new file updates
+        await new Promise((resolve, reject) => {
+            exec(`git commit -m "Update FAST Tooling dependencies"`, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    reject();
+                }
+        
+                if (stderr) {
+                    console.error(`stderr: ${stderr}`);
+                    reject();
+                }
+
+                resolve();
+            });
+        }).catch((reason) => {
+            console.error(reason);
+        });
+    }
+}
+
+updateDependencies();
